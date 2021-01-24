13a26dfac0086919e00bd4aae0fc5179088a3c47
bee-dmg <69877469+bee-dmg@users.noreply.github.com>
2021-01-24 10:38:29 -0500

    Add files via upload

--- Magic/js/mine/app.js
+++ Magic/js/mine/app.js
@@ -5,7 +5,14 @@ const p2ButtonMinus = document.querySelector("#p2ButtonMinus");
 const p1Display = document.querySelector("#p1Display");
 const p2Display = document.querySelector("#p2Display");
 const reset = document.querySelector("#reset");
-
+const p2NameIn = document.querySelector("#p2NameIn");
+const p2Name = document.querySelector("#p2Name");
+const p1NameIn = document.querySelector("#p1NameIn");
+const p1Name = document.querySelector("#p1Name");
+const p1Submit = document.querySelector("#p1Submit");
+const p2Submit = document.querySelector("#p2Submit");
+const p1ChangeName = document.querySelector("#p1ChangeName");
+const p2ChangeName = document.querySelector("#p2ChangeName");
 
 
 let p1Score = 20;
@@ -94,3 +101,36 @@ function resetFunc(){
         p1Display.classList.remove('winner', 'loser');
         p2Display.classList.remove('loser', 'winner');
 }
+
+p2NameIn.addEventListener('input', function(){
+    p2Name.innerText = p2NameIn.value;
+
+})
+
+p1NameIn.addEventListener('input', function(){
+    p1Name.innerText = p1NameIn.value;
+
+})
+
+p1Submit.addEventListener('click', function(){
+    p1NameIn.classList.add('hide');
+    p1Submit.classList.add('hide');
+})
+
+p2Submit.addEventListener('click', function(){
+    p2NameIn.classList.add('hide');
+    p2Submit.classList.add('hide');
+})
+
+p1ChangeName.addEventListener('click', function(){
+    p1NameIn.classList.remove('hide');
+    p1Submit.classList.remove('hide');
+
+})
+
+p2ChangeName.addEventListener('click', function(){
+    p2NameIn.classList.remove('hide');
+    p2Submit.classList.remove('hide');
+
+})
+
